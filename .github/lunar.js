74a3d0124f6c3e0492a89d14cff8f9d6b7e2b579
wilmahjortas <hjwi22oe@student.ju.se>
2023-03-02 14:45:38 +0100

    update

--- .github/lunar.js
+++ .github/lunar.js
@@ -13,34 +13,34 @@ for (let i = 0; i < 1100; i++) {
 }
 
 let bigStarX = 100;
-let bigStarY = 100;
-let scaleStar = 1;
+let bigStarY = 220;
+let scaleStar = 0.15;
 
-function star() {
+function yellowStar() {
   noStroke();
   fill(250, 253, 15);
   triangle(
-    bigStarX + 100 * s,
-    bigStarY - 60 * s,
-    bigStarX + 200 * s,
-    bigStarY + 100 * s,
+    bigStarX + 100 * scaleStar,
+    bigStarY - 60 * scaleStar,
+    bigStarX + 200 * scaleStar,
+    bigStarY + 100 * scaleStar,
     bigStarX,
-    bigStarY + 100 * s
+    bigStarY + 100 * scaleStar
   );
   triangle(
-    bigStarX + 5 * s,
-    bigStarY - 5 * s,
-    bigStarX + 195 * s,
-    bigStarY - 10 * s,
-    bigStarX + 100 * s,
-    bigStarY + 150 * s
+    bigStarX + 5 * scaleStar,
+    bigStarY - 5 * scaleStar,
+    bigStarX + 195 * scaleStar,
+    bigStarY - 10 * scaleStar,
+    bigStarX + 100 * scaleStar,
+    bigStarY + 150 * scaleStar
   );
 }
 
 function ufo(x, y) {
   //spaceship;
   stroke(0, 0, 0);
-  fill(0, 255, 0, 130);
+  fill(0, 255, 0);
   ellipse(x, y, 100 * s, 40 * s);
 
   fill(135, 206, 235);
@@ -86,34 +86,45 @@ function ufo(x, y) {
 let x = 100;
 let y = 100;
 let s = 1;
+let moonS = 2;
 
 function moon(x, y) {
-  // stroke(231,207,138);
   fill(252, 238, 167);
-  ellipse(x, y + 230 * s, 170 * s);
+  ellipse(x, y + 230 * moonS, 170 * moonS);
   fill(231, 207, 138);
-  ellipse(x - 35 * s, y + 185 * s, 40 * s, 35 * s);
-  ellipse(x + 35 * s, y + 190 * s, 30 * s, 35 * s);
-  ellipse(x + 5 * s, y + 240 * s, 25 * s);
-  ellipse(x - 53 * s, y + 250 * s, 37 * s, 40 * s);
-  ellipse(x + 32 * s, y + 280 * s, 42 * s, 37 * s);
-  ellipse(x + 60 * s, y + 230 * s, 20 * s, 24 * s);
+  ellipse(x - 35 * moonS, y + 185 * moonS, 40 * moonS, 35 * moonS);
+  ellipse(x + 35 * moonS, y + 190 * moonS, 30 * moonS, 35 * moonS);
+  ellipse(x + 5 * moonS, y + 240 * moonS, 25 * moonS);
+  ellipse(x - 53 * moonS, y + 250 * moonS, 37 * moonS, 40 * moonS);
+  ellipse(x + 32 * moonS, y + 280 * moonS, 42 * moonS, 37 * moonS);
+  ellipse(x + 60 * moonS, y + 230 * moonS, 20 * moonS, 24 * moonS);
+  ellipse(x + 8 * moonS, y + 160 * moonS, 24 * moonS, 18 * moonS);
 }
 
-let moonX = 700;
+let moonX = 100;
 let ufoY = 100;
+let ufoX = 100;
 let velocity = 1;
 let acceleration = 0.2;
 let speed = 0;
 let isGameActive = false;
 
-function draw() {
-  noStroke();
+function startScreen() {
   background(0, 0, 0);
+  fill(255, 255, 0);
+  rect(120, 150, 350, 200);
+  fill(255, 0, 255);
+  text("Press space to start game", 230, 245);
+  text("Use arrowkeys to land on the moon, watch out for the stars", 140, 270);
 
   if (keyIsDown(32)) {
-    isGameActive = true;
+    state = "game";
   }
+}
+
+function gameScreen() {
+  noStroke();
+  background(0, 0, 0);
 
   for (let index in starX) {
     fill(255, 255, 255, Math.abs(Math.sin(starAlpha[index])) * 255);
@@ -121,23 +132,21 @@ function draw() {
     starAlpha[index] = starAlpha[index] + 0.02;
   }
 
-  moon(moonX, 100);
-  ufo(x, ufoY);
+  yellowStar(bigStarX, bigStarY);
+  moon(moonX + 200, 85);
+  ufo(ufoX, ufoY);
 
   if (isGameActive) {
-    moonX = moonX - 4;
     ufoY = ufoY + velocity;
     velocity = velocity + acceleration;
     x = x + speed;
 
-    if (moonX < -100) {
-      moonX = width + 100;
+    if (keyIsDown(32)) {
+      isGameActive = true;
     }
-
     if (keyIsDown(38)) {
       velocity = velocity - 0.5;
     }
-
     if (keyIsDown(39)) {
       speed = 5;
     } else if (keyIsDown(37)) {
@@ -145,9 +154,43 @@ function draw() {
     } else {
       speed = 0;
     }
-
-    if (ufoY > 280) {
-      isGameActive = false;
+  }
+  /*     if (ufoY > 390 && ufoX < 160 && ufoX > 440) {
+      
     }
+  }
+
+  if (ufoY > 390 && ufoX > 160 && ufoX < 410) {
+    winScreen();
+  } */
+}
+
+function gameOverScreen() {
+  background(0, 0, 0);
+  fill(255, 0, 0);
+  rect(120, 150, 350, 200);
+  fill(0, 0, 0);
+  text("GAME OVER", 260, 240);
+  text("Press space to try again", 230, 270);
+}
+
+function winScreen() {
+  background(0, 0, 0);
+  fill(0, 255, 0);
+  rect(120, 130, 350, 200);
+  fill(0, 0, 0);
+  text("YOU WIN", 260, 220);
+  text("Press space to try again", 225, 250);
+}
+
+let state = "start";
+
+function draw() {
+  if (state === "start") {
+    startScreen();
+  } else if (state === "game") {
+    gameScreen();
+  } else if (state === "result") {
+    gameOverScreen();
   }
 }
